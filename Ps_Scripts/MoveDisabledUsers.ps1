867270ea56ec27ef1f31dca305821cc6d47add17
Anthony DeAngelis <117580657+antgotgithub@users.noreply.github.com>
2025-09-07 09:52:21 -0700

    Delete Ps_Scripts directory

--- Ps_Scripts/MoveDisabledUsers.ps1
+++
@@ -1,9 +0,0 @@
-﻿#Import AD odule
-Import-Module ActiveDirectory
-
-#List all AD disbaled users
-Search-ADAccount -AccountDisabled | Where {$_.DistinguishedName -notlike "OU=Disabled Users"} | Select-Object Name, DistinguishedName
-
-#Move all disabled users to own ou
-Search-ADAccount -AccountDisabled | Move-ADObject -TargetPath "OU=Disabled Users,OU=Antdomain,DC=Antdomain,DC=com"
-
