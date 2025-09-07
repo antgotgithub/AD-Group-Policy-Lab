4939825f6811555421824233ed3d8847686c5de9
Anthony DeAngelis <117580657+antgotgithub@users.noreply.github.com>
2025-09-07 12:37:48 -0400

    Add files via upload

---
+++ Ps_Scripts/NewADUser.ps1
@@ -0,0 +1,23 @@
+﻿# Import AD module
+Import-Module ActiveDirectory
+
+# Grab variables from user
+$firstname = Read-Host -Prompt "Enter first name"
+$lastname  = Read-Host -Prompt "Enter last name"
+
+# Build account name (capital first initial + lastname)
+$firstInitial = $firstname.Substring(0,1).ToUpper()
+$sam = $firstInitial + $lastname
+$upn = "$sam@antdomain.com"
+
+# Create AD User
+New-ADUser `
+    -Name "$firstname $lastname" `
+    -GivenName $firstname `
+    -Surname $lastname `
+    -SamAccountName $sam `
+    -UserPrincipalName $upn `
+    -AccountPassword (ConvertTo-SecureString "Admin12$" -AsPlainText -Force) `
+    -Path "OU=Domain users,OU=Antdomain,DC=Antdomain,DC=com" `
+    -ChangePasswordAtLogon $true `
+    -Enabled $true
---
+++ Ps_Scripts/UserCSV Read.ps1
@@ -0,0 +1,32 @@
+﻿# Import the AD Module
+Import-Module ActiveDirectory
+
+# Get the path to our target CSV file
+$filepath = Read-Host -Prompt "Please enter the path to the CSV file that contains the new user accounts"
+
+# Import the CSV as an array
+$users = Import-CSV $filepath
+
+# Complete an action for each user in the CSV file
+ForEach ($user in $users) {
+    # Build the UPN: First initial (capitalized) + Lastname (capitalized) + @Antdomain.com
+    $firstInitial = $user.'First Name'.Substring(0,1).ToUpper()
+    $lastName = ($user.'Last Name').Substring(0,1).ToUpper() + ($user.'Last Name').Substring(1).ToLower()
+    $upn = $firstInitial + $lastName + "@Antdomain.com"
+
+    # Create the AD user
+    New-ADUser `
+        -Name ($user.'First Name' + " " + $user.'Last Name') `
+        -GivenName $user.'First Name' `
+        -Surname $user.'Last Name' `
+        -UserPrincipalName $upn `
+        -SamAccountName ($firstInitial + $lastName) `
+        -AccountPassword (ConvertTo-SecureString "Admin12$" -AsPlainText -Force) `
+        -Description $user.Description `
+        -EmailAddress $user.'Email Address' `
+        -Title $user.'Job Title' `
+        -OfficePhone $user.'Office Phone' `
+        -Path "OU=Domain users,OU=Antdomain,DC=Antdomain,DC=com" `
+        -ChangePasswordAtLogon $true `
+        -Enabled ([System.Convert]::ToBoolean($user.Enabled))
+}
